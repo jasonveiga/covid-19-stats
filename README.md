5557717af194a2799816215011cf90f209732153
Jason Veiga <jasonveiga@me.com>
2020-04-03 08:34:54 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # covid-19-stats
-a hack-job python script for showing COVID-19 stats
+This is just a quick, hacky python script for parsing and displaying graphs from JHU's
+COVID-19 data. It's not designed to be especially robust or portable.
+
+# Getting the data
+
+Git submodules are used to grab the data from
+[here](https://github.com/CSSEGISandData/COVID-19.git). To get everything at
+once you can do:
+```sh
+git clone --recurse-submodules https://github.com/jasonveiga/covid-19-stats.git
+```
+
+If you want to refresh the data, run:
+
+```sh
+git submodule update --recursive --remote
+```
+
+# Script setup
+
+The script (covid-stats.py) requires matplotlib to run. If you want to quickly
+set up a virtual environment to run it, here's an example (assumes you're in the
+base directory for this repo).
+
+```sh
+virtualenv -p python3 venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
+
+Then, just run the script:
+```sh
+./covid-19-stats.py
+```
---
+++ covid-stats.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+import os
+import csv
+import re
+import datetime
+import matplotlib.pyplot as plt
+
+
+def auto_int(x):
+    if not x:
+        return 0
+    else:
+        return int(x)
+
+
+def process_file(df):
+    stats_by_country = {}
+
+    rows = list(csv.reader(df))
+    confirmed_index = 0
+    recovered_index = 0
+    country_index = 0
+    deaths_index = 0
+
+    for i, cname in enumerate(rows[0]):
+        if re.match('country', cname, re.I):
+            country_index = i
+        elif re.match('death', cname, re.I):
+            deaths_index = i
+        elif re.match('confirmed', cname, re.I):
+            confirmed_index = i
+        elif re.match('recovered', cname, re.I):
+            recovered_index = i
+
+    for row in rows[1:]:
+        country = row[country_index]
+        deaths = auto_int(row[deaths_index])
+        confirmed = auto_int(row[confirmed_index])
+        recovered = auto_int(row[recovered_index])
+
+        if country not in stats_by_country:
+            stats_by_country[country] = {
+                "confirmed": 0,
+                "recovered": 0,
+                "deaths": 0
+            }
+
+        stats_by_country[country]["deaths"] += deaths
+        stats_by_country[country]["confirmed"] += confirmed
+        stats_by_country[country]["recovered"] += recovered
+
+    return stats_by_country
+
+
+data_dir = "COVID-19/csse_covid_19_data/csse_covid_19_daily_reports"
+all_files = sorted(fn for fn in os.listdir(data_dir) if fn.endswith("csv"))
+stats = [(f[0:-4], process_file(open(data_dir + "/" + f))) for f in all_files]
+
+for f, stat in stats[-15:]:
+    print("US confirmed/deaths/recovered for {}: {}/{}/{}".format(
+        f, stat['US']['confirmed'], stat['US']['deaths'],
+        stat['US']['recovered']))
+
+y1 = [stat['US']['deaths'] for f, stat in stats[-20:]]
+y2 = [stat['US']['confirmed'] for f, stat in stats[-20:]]
+x = [datetime.datetime.strptime(f, "%m-%d-%Y") for f, stat in stats[-20:]]
+fig, axs = plt.subplots(2)
+axs[0].plot(x, y1)
+axs[1].plot(x, y2)
+
+for ax in axs:
+    for tick in ax.get_xticklabels():
+        tick.set_rotation(45)
+
+plt.tight_layout()
+plt.show()
